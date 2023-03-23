8f2c5dffdfddbf65a6f748457f22dbe4667bbcb6
jaewook718 <102957590+jaewook718@users.noreply.github.com>
2023-03-23 15:14:24 +0900

    [Gold III] Title: 파티, Time: 1840 ms, Memory: 34348 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.py
@@ -0,0 +1,41 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+
+def dijkstra(start):
+    distance = [1e9] * (N + 1)
+    distance[start] = 0
+    heapq.heappush(heap, (0, start))
+    while heap:
+        weight, now = heapq.heappop(heap)
+
+        if distance[now] < weight:
+            continue
+
+        for w, v in graph[now]:
+            tmp = weight + w
+            if tmp < distance[v]:
+                distance[v] = tmp
+                heapq.heappush(heap, (tmp, v))
+
+    return distance
+
+
+N, M, X = map(int,input().split())
+
+
+heap = []
+graph = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    u, v, w = map(int,input().split())
+    graph[u].append((w, v))
+
+ans = 0
+for i in range(1, N+1):
+    go = dijkstra(i)
+    back = dijkstra(X)
+    ans = max(ans, go[X] + back[i])
+
+print(ans)
