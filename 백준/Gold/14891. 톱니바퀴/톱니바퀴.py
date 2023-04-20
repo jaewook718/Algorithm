d3d62e77aeac73db052e70845c2a08a971cf5a19
jaewook718 <102957590+jaewook718@users.noreply.github.com>
2023-04-20 17:31:13 +0900

    [Gold V] Title: 톱니바퀴, Time: 148 ms, Memory: 114432 KB -BaekjoonHub

---
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -0,0 +1,47 @@
+from collections import deque
+def rotateleft(x):
+    x.append(x.popleft())
+
+
+def rotateright(x):
+    x.appendleft(x.pop())
+
+
+def rotate(x,command):
+    qu = deque()
+    visited = [0]*4
+    visited[x] = 1
+    qu.append((x,command))
+    while qu:
+        a, b = qu.popleft()
+        if b == 1:
+            rotateright(q[a])
+        elif b == -1:
+            rotateleft(q[a])
+        for i in [1, -1]:
+            na = a + i
+            if 0<=na<4 and visited[na] == 0:
+                if na > a and a in tmp:
+                    qu.append((na,-b))
+                    visited[na] = 1
+                elif na < a and na in tmp:
+                    qu.append((na,-b))
+                    visited[na] = 1
+
+
+q = []
+ans = 0
+for _ in range(4):
+    q.append(deque(list(map(int,input()))))
+K = int(input())
+com = [list(map(int, input().split())) for _ in range(K)]
+for n, c in com:
+    tmp = []
+    for i in range(3):
+        if q[i][2] != q[i+1][-2]:
+            tmp.append(i)
+    rotate(n-1, c)
+for i in range(4):
+    if q[i][0] == 1:
+        ans += 2**i
+print(ans)
