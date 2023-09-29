d28755d0a94318b8862c94fe86c309bafe69324f
jaewook718 <102957590+jaewook718@users.noreply.github.com>
2023-09-29 23:52:28 +0900

    Update 이중우선순위큐.py

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,24 +1,21 @@
 import heapq
 def solution(operations):
-    max_heap = []
     heap = []
     for operation in operations:
         cmd, num = operation.split()
         num = int(num)
         if cmd == "I":
             heapq.heappush(heap, num)
-            heapq.heappush(max_heap, [num*-1,num])
         elif cmd == "D":
             if len(heap)==0:
                 continue
             if num == -1:
                 min_value = heapq.heappop(heap)
-                max_heap.remove([min_value*-1,min_value])
             else:
-                max_value = heapq.heappop(max_heap)[1]
+                max_value = max(heap)
                 heap.remove(max_value)
     if heap:
-        return [heapq.heappop(max_heap)[1],heapq.heappop(heap)]
+        return [max(heap),heapq.heappop(heap)]
     else:
         return [0, 0]
     return answer
